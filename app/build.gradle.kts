b88ddd05697a08175ffe604a9c4fedea11ba8c59
lwl <18883278692@163.com>
2026-06-29 18:12:19 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,8 +83,8 @@ android {
         applicationId = "com.cla.clip.master"
         minSdk = 24
         targetSdk = 36
-        versionCode = 32
-        versionName = "0.5.1"
+        versionCode = 33
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
